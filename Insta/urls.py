9db3f188e70f55cdb9e1dc65290c4a4c9d72313a
SSSYJ <songyjoy@gmail.com>
2020-01-27 18:11:33 +0800

    CRUD & Authentication[default userview] & Authorization[mixin]

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView,PostDeleteView, SignUp
 
 urlpatterns = [
-    path('', HelloWorld.as_view(), name='helloWorld'),
+    path('', HelloWorld.as_view(), name = 'helloWorld'),
+    path('posts/', PostsView.as_view(), name = 'posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name = 'post_detail'),
+    path('post/new/', PostCreateView.as_view(), name = 'post_create'),
+    path('post/update/<int:pk>/', PostUpdateView.as_view(), name = 'post_update'),
+    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name = 'post_delete'),
+    path('auth/signup/', SignUp.as_view(), name = 'sign_up'),
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,4 +1,42 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Post
 
 class HelloWorld(TemplateView): # HelloWorld is-a TempateView
     template_name = 'test.html'
+
+
+class PostsView(ListView):
+    model = Post
+    template_name = 'index.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+    login_url = 'login'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")
+    form_class = UserCreationForm
